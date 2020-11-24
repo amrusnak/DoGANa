787e9fd672c0fff54ca301322bdb958ce5885c4f
Alexander Michael Rusnak <rusnak@iccluster131.iccluster.epfl.ch>
2020-11-24 01:45:19 +0100

    fix import spectral and probs some other stuff

--- Model/models/networks.py
+++ Model/models/networks.py
@@ -6,7 +6,7 @@ import functools
 from torch.optim import lr_scheduler
 import numpy as np
 from .stylegan_networks import StyleGAN2Discriminator, StyleGAN2Generator, TileStyleGAN2Discriminator
-from spectral import SpectralNorm
+from .spectral import SpectralNorm
 
 ###############################################################################
 # Helper Functions
@@ -125,7 +125,7 @@ def get_norm_layer(norm_type='instance'):
     elif norm_type == 'instance':
         norm_layer = functools.partial(nn.InstanceNorm2d, affine=False, track_running_stats=False)
     elif norm_type == 'spectral':
-        norm_layer = functools.partial(SpectralNorm, affine=False, track_running_stats=False)
+        norm_layer = SpectralNorm
     elif norm_type == 'none':
         def norm_layer(x):
             return Identity()
