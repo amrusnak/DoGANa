f69d8ef41f559a75b46a75c59b7ce27c1ac4139f
Alexander Rusnak <amrusnak2013@gmail.com>
2020-11-26 16:53:36 +0100

    lol

--- Model/models/networks.py
+++ Model/models/networks.py
@@ -251,7 +251,7 @@ def define_G(input_nc, output_nc, ngf, netG, norm='instance', selfAttn=False, us
     norm_layer = get_norm_layer(norm_type=norm)
 
     if netG == 'resnet_9blocks':
-        net = ResnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, no_antialias=no_antialias, no_antialias_up=no_antialias_up, n_blocks=9, opt=opt, norm_type=norm)
+        net = ResnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, no_antialias=no_antialias, no_antialias_up=no_antialias_up, n_blocks=9, opt=opt, norm_type=norm, selfAttn=selfAttn)
     elif netG == 'resnet_6blocks':
         net = ResnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, no_antialias=no_antialias, no_antialias_up=no_antialias_up, n_blocks=6, opt=opt)
     elif netG == 'resnet_4blocks':
