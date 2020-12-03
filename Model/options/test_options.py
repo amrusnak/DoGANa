a33cb6c9905cc8fa927bd449cd8e3a0ab69eb9be
Mariko Makhmutova <mariko.makhm@gmail.com>
2020-12-03 15:42:36 +0100

    add dogana test

--- Model/options/test_options.py
+++ Model/options/test_options.py
@@ -15,6 +15,9 @@ class TestOptions(BaseOptions):
         parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
         parser.add_argument('--num_test', type=int, default=50, help='how many test images to run')
 
+        # DoGANa model testing
+        parser.add_argument('--dogana', type=str, default='cpaint', help='DoGANa model: cpaint, bwpaint, bwdraw, underwater')
+
         # To avoid cropping, the load_size should be the same as crop_size
         parser.set_defaults(load_size=parser.get_default('crop_size'))
         self.isTrain = False
